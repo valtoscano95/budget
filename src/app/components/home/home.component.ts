a72cca49ad435ed18f5a28eb34000be407f6e26c
Valeria Toscano <valeriatoscano@Valerias-MacBook-Pro.local>
2024-01-07 14:47:57 -0600

    adapted login and home for mobile

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
   selector: 'home-container',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent{
 
+  constructor(private authenticationService: AuthenticationService){}
+
+  login(){
+    this.authenticationService.activateLogin();
+  }
+
+  signup(){
+    this.authenticationService.activateSignUp();
+  }
 }
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,21 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit, OnDestroy {
+
+  constructor(private authService: AuthenticationService){}
+  isLogin:boolean;
+  isLoginSubscription: Subscription;
+  title = 'Login';
 
   LoginForm = new FormGroup({
-    email: new FormControl('', Validators.required),
-    password: new FormControl(null, Validators.required),
-
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl(null, [Validators.required, Validators.min(8)]),
   });
 
   onSubmit(){
 
+  }
+
+  ngOnInit(): void {
+    this.isLoginSubscription = this.authService.isLogin.subscribe((item)=>{
+      this.isLogin = item;
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.isLoginSubscription.unsubscribe();
   }
 
 }
